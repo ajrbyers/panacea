1a59d530820003c270731fa6ec08796eccf6358f
Andy Byers <andy.byers@openlibhums.org>
2017-11-21 16:53:19 +0000

    Added basic app structure.

--- src/core/example_settings.py
+++ src/core/example_settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'journal',
+    'review',
+    'moderator',
 ]
 
 MIDDLEWARE = [
